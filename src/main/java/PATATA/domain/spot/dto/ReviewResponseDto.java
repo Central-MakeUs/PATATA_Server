a4d6d3c64bb209a1b73e20de8bfd6d8aa3e2c5b8
do-dop <pdy0207@naver.com>
2025-02-04 00:12:50 +0900

    fix: 리뷰 생성일 반환 포맷 변경

--- src/main/java/PATATA/domain/spot/dto/ReviewResponseDto.java
+++ src/main/java/PATATA/domain/spot/dto/ReviewResponseDto.java
@@ -1,5 +1,6 @@
 package PATATA.domain.spot.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,6 +12,7 @@ import java.time.LocalDateTime;
 public class ReviewResponseDto {
     private Long reviewId;
     private String reviewText;
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime reviewDate;
 
     @Builder
