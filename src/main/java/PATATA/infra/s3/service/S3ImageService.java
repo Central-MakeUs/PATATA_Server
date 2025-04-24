38fb589c045365628a99ed1ff80f3d94aafad23e
do-dop <pdy0207@naver.com>
2025-04-24 16:29:51 +0900

    fix: spot image lambda 리사이징

--- src/main/java/PATATA/infra/s3/service/S3ImageService.java
+++ src/main/java/PATATA/infra/s3/service/S3ImageService.java
@@ -81,49 +81,8 @@ public class S3ImageService {
         return S3ImageUrlDto.of(originalUrl, resizedUrl400, null, null);
     }
 
-    private String uploadThumbnailImage(MultipartFile image, String folder) throws IOException {
-        log.info("JVM Heap - Max: {} MB, Total: {} MB, Free: {} MB",
-                Runtime.getRuntime().maxMemory() / (1024 * 1024),
-                Runtime.getRuntime().totalMemory() / (1024 * 1024),
-                Runtime.getRuntime().freeMemory() / (1024 * 1024));
 
-        ImageInputStream iis = ImageIO.createImageInputStream(image.getInputStream());
-        Iterator<ImageReader> readers = ImageIO.getImageReaders(iis);
-
-        if (!readers.hasNext()) {
-            throw new IllegalArgumentException("지원하지 않는 이미지 형식입니다.");
-        }
-
-        ImageReader reader = readers.next();
-        reader.setInput(iis, true);
-        BufferedImage bufferedImage = reader.read(0);
-
-        ByteArrayOutputStream os = new ByteArrayOutputStream();
-        Thumbnails.of(bufferedImage)
-                .size(400, 400)
-                .outputFormat("jpg")
-                .outputQuality(0.8f)
-                .toOutputStream(os);
-
-        byte[] thumbnailBytes = os.toByteArray();
-        ByteArrayInputStream inputStream = new ByteArrayInputStream(thumbnailBytes);
-
-        String thumbnailFileName = UUID.randomUUID().toString().concat("_thumbnail.jpg");
-
-        ObjectMetadata metadata = new ObjectMetadata();
-        metadata.setContentLength(thumbnailBytes.length);
-        metadata.setContentType("image/jpeg");
-
-        amazonS3.putObject(new PutObjectRequest(bucket, folder + thumbnailFileName, inputStream, metadata));
-
-        iis.close();
-        os.close();
-        inputStream.close();
-
-        return amazonS3.getUrl(bucket, folder + thumbnailFileName).toString();
-    }
-
-    // ----------------------------------------
+    // ---------------------------------------- 스팟 이미지 --------------------------------
 
     public S3ImageUrlDto upload(MultipartFile image, String folder) {
         //입력받은 이미지 파일이 빈 파일인지 검증
@@ -145,63 +104,33 @@ public class S3ImageService {
     }
 
     private S3ImageUrlDto uploadOriginalAndResizedImage(MultipartFile image, String folder) throws IOException {
-        String uuid = UUID.randomUUID().toString();
-        String baseFileName = folder + uuid;
+        String s3FileName = UUID.randomUUID().toString().concat(".jpg");
 
-        // 원본 업로드
-        String originalFileName = baseFileName + "_o.jpg";
-        String originalUrl;
-        try (InputStream originalInputStream = image.getInputStream()) {
-            originalUrl = uploadImageToS3(originalInputStream, originalFileName, image.getSize());
-        }
+        InputStream is = image.getInputStream();
+        byte[] bytes = IOUtils.toByteArray(is);
 
-        // 리사이징 이미지 생성 및 업로드
-        Map<Integer, String> resizedUrls = new HashMap<>();
-        int[] sizes = {400, 800, 1200};
-
-        for (int size : sizes) {
-            String resizedFileName = baseFileName + "_r" + size + ".jpg";
-
-            try (
-                    InputStream resizeInputStream = image.getInputStream();
-                    ByteArrayOutputStream resizedOutputStream = new ByteArrayOutputStream()
-            ) {
-                Thumbnails.of(resizeInputStream)
-                        .size(size, size)
-                        .outputFormat("jpg")
-                        .outputQuality(0.8)
-                        .toOutputStream(resizedOutputStream);
-
-                byte[] resizedBytes = resizedOutputStream.toByteArray();
-                String resizedUrl = uploadImageToS3(new ByteArrayInputStream(resizedBytes), resizedFileName, resizedBytes.length);
-                resizedUrls.put(size, resizedUrl);
-            }
-        }
-
-        return new S3ImageUrlDto(
-                originalUrl,
-                resizedUrls.get(400),
-                resizedUrls.get(800),
-                resizedUrls.get(1200)
-        );
-    }
-
-    //S3에 파일 업로드
-    private String uploadImageToS3(InputStream inputStream, String key, long contentLength) throws IOException {
         //metadata 생성
         ObjectMetadata metadata = new ObjectMetadata();
-        metadata.setContentLength(contentLength);
+        metadata.setContentLength(bytes.length);
         metadata.setContentType("image/jpeg");
 
-//        //S3에 요청할 때 사용할 byteInputStream 생성
-//        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
+        //S3에 요청할 때 사용할 byteInputStream 생성
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
 
         //S3로 putObject 할 때 사용할 요청 객체
-        PutObjectRequest putObjectRequest = new PutObjectRequest(bucket, key, inputStream, metadata);;
+        PutObjectRequest putObjectRequest = new PutObjectRequest(bucket, folder + "/" + s3FileName, byteArrayInputStream, metadata);
 
         //put image to S3
         amazonS3.putObject(putObjectRequest);
-        return amazonS3.getUrl(bucket, key).toString();
-    }
 
+        byteArrayInputStream.close();
+        is.close();
+
+        String originalUrl = amazonS3.getUrl(bucket, folder + "/" + s3FileName).toString();
+        String resizedUrl400 = amazonS3.getUrl(bucket, folder + "_resized/resized_400_" + s3FileName).toString();
+        String resizedUrl800 = amazonS3.getUrl(bucket, folder + "_resized/resized_800_" + s3FileName).toString();
+        String resizedUrl1200 = amazonS3.getUrl(bucket, folder + "_resized/resized_1200_" + s3FileName).toString();
+
+        return S3ImageUrlDto.of(originalUrl, resizedUrl400, resizedUrl800, resizedUrl1200);
+    }
 }
